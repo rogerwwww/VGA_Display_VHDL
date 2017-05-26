bb16f3257f1d9b42c7cf8f1fc4068aef99c3ebe7
Roger Wang <liberatetheus@hotmail.com>
2017-05-26 19:35:32 +0800

    Tiny bugs fixed

--- heart_generator.py
+++ heart_generator.py
@@ -1,11 +1,14 @@
 from math import *
 
 q = []
+# flag shows if the current point is already in range.
+# if flag changes from 0 to 1, it shows that we have just entered the range;
+# if flag changes from 1 to 0, it shows that we have just left the range.
 flag = 0
 
 for v in range(0, 350):
     for h in range(439, 839):
-        print pow(((h - 639) / 80.0), 2) + pow((-(v - 171) / 80.0 - pow(pow(((h - 639) / 80.0), 2), 1.0 / 3)), 2)
+        # this expression calculates the range of heart. 639 and 171 are the offset values.
         if pow(((h - 639) / 80.0), 2) + pow((-(v - 171) / 80.0 - pow(pow(((h - 639) / 80.0), 2), 1.0 / 3)), 2) < 1:
             if flag == 0:
                 temp = [v, h]
@@ -14,7 +17,9 @@ for v in range(0, 350):
             q.append([temp[0], temp[1], h])
             flag = 0
 
+# write the result to "out.txt"
 f = open('out.txt', 'w')
+
 for i in q:
     f.write('(vCounter = ')
     f.write('%d' % i[0])
